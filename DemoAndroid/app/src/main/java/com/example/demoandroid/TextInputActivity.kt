5c5b55d08e2e7f74e24078b3536f09bc53a717fd
Lucle <73305887+LEVIETLUC@users.noreply.github.com>
2024-08-01 23:57:39 -0700

    fix: set ui to the center of the screen (#3)

--- DemoAndroid/app/src/main/java/com/example/demoandroid/TextInputActivity.kt
+++ DemoAndroid/app/src/main/java/com/example/demoandroid/TextInputActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 
@@ -17,6 +19,7 @@ class TextInputActivity : AppCompatActivity() {
         init()
         val dataText = intent.getStringExtra("data text")
         editTextData.hint = dataText
+        isEmptyEditText(editTextData)
         buttonBack.setOnClickListener {
             val textData = editTextData.text.toString()
             senData(textData)
@@ -30,11 +33,26 @@ class TextInputActivity : AppCompatActivity() {
         editTextData = findViewById(R.id.edit_data_text)
     }
 
-
-
     private fun senData(textData: String){
         val resultIntent = Intent()
         resultIntent.putExtra("data text response", textData)
         setResult(Activity.RESULT_OK, resultIntent)
+    }
+
+    private fun isEmptyEditText(editText: EditText) {
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Không cần thực hiện gì ở đây
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Kiểm tra xem EditText có dữ liệu không
+                buttonBack.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Không cần thực hiện gì ở đây
+            }
+        })
     }
 }
