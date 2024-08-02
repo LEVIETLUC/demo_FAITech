a143109d565af4f1ab1802c3760e8b1eac46d8be
Lucle <11lucviet@gmail.com>
2024-08-02 10:20:26 +0700

    fix: change the way of data transmission

--- DemoAndroid/app/src/main/java/com/example/demoandroid/TextInputActivity.kt
+++ DemoAndroid/app/src/main/java/com/example/demoandroid/TextInputActivity.kt
@@ -1,5 +1,6 @@
 package com.example.demoandroid
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,17 +14,27 @@ class TextInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_input)
+        init()
         val dataText = intent.getStringExtra("data text")
-        findViewById<EditText>(R.id.edit_data_text).setHint(dataText)
-        buttonBack = findViewById(R.id.button_back)
-        editTextData = findViewById(R.id.edit_data_text)
+        editTextData.hint = dataText
         buttonBack.setOnClickListener {
             val textData = editTextData.text.toString()
-            Intent(this, TextViewActivity::class.java).also {
-                it.putExtra("data text response", textData)
-                startActivity(it)
-            }
+            senData(textData)
+            finish()
         }
 
+    }
+
+    private fun init(){
+        buttonBack = findViewById(R.id.button_back)
+        editTextData = findViewById(R.id.edit_data_text)
+    }
+
+
+
+    private fun senData(textData: String){
+        val resultIntent = Intent()
+        resultIntent.putExtra("data text response", textData)
+        setResult(Activity.RESULT_OK, resultIntent)
     }
 }
--- DemoAndroid/app/src/main/java/com/example/demoandroid/TextViewActivity.kt
+++ DemoAndroid/app/src/main/java/com/example/demoandroid/TextViewActivity.kt
@@ -5,35 +5,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class TextViewActivity : AppCompatActivity() {
 
     private lateinit var buttonNext : Button
     private lateinit var textView : TextView
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_view)
-        buttonNext = findViewById(R.id.button_next)
-        textView  = findViewById(R.id.text_view_data)
+        init()
         inputText("Hello")
-        val dataText = intent.getStringExtra("data text response")
-        if(dataText != null) {
-            inputText(dataText)
-        }
-        else
-        {
-            inputText("Hello")
-        }
+        reciveData()
+
         buttonNext.setOnClickListener {
-            val textData = textView.text.toString()
-            Intent(this, TextInputActivity::class.java).also {
-                it.putExtra("data text", textData)
-                startActivity(it)
-            }
+            senData(textView.text.toString())
         }
     }
 
-    fun inputText(putName : String) {
+    private fun init(){
+        buttonNext = findViewById(R.id.button_next)
+        textView  = findViewById(R.id.text_view_data)
+    }
+
+    private fun senData(textData : String){
+        val intent = Intent(this, TextInputActivity::class.java)
+        intent.putExtra("data text", textData)
+        resultLauncher.launch(intent)
+    }
+
+    private fun reciveData(){
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                val textData = data?.getStringExtra("data text response")
+                inputText(textData.toString())
+            }
+            else
+            {
+                inputText("Hello")
+            }
+        }    }
+
+    private fun inputText(putName : String) {
         textView.text = putName
     }
 }
