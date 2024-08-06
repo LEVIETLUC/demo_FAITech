8e222e5cc8adf50ff4c42e14e87e41d7ae2d6072
Lucle <73305887+LEVIETLUC@users.noreply.github.com>
2024-08-06 01:58:56 -0700

    feat: add custom ViewGroup (#10)

---
+++ DemoAndroid/app/src/main/java/com/example/demoandroid/activities/viewgroup/CustomViewGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demoandroid.activities.viewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.demoandroid.R
+
+class CustomViewGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_view_group)
+    }
+}
